fba6132635530c1352b10bffa3770d95916f0ecc
BrianOMora <brian_1321124075@uptecamac.edu.mx>
2021-09-08 11:19:24 -0500

    Viaje a universidad

---
+++ viaje_universidad.js
@@ -0,0 +1,24 @@
+/*
+1.- Salir de casa
+2.- Caminar a la parada de Mexibus
+3.- Tomar Mexibus
+4.- Bajar en estación Cuathemoc Norte
+5.- Tomar Combi o Camion
+6.- Bajar en el paradero de Tecámac
+7.- Caminar a la universidad
+8.- Entrar a la universidad
+*/
+
+var transporte = "carro";
+
+console.log("Paso 1: Salir a la entrada de mi casa");
+
+if(transporte == "combi"){
+    console.log("Paso 2: Subirme a la combi");
+}else{
+    console.log("Paso 2: Subirme al transporte publico");
+}
+
+console.log("Paso 3: Bajar en paradero de Tecámac");
+console.log("Paso 4: Caminar a la entrada de la uni");
+console.log("Paso 5: Caminar al aula");
