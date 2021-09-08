75325b6a0e4469d747a1846498f2eeb2df1a7898
BrianOMora <brian_1321124075@uptecamac.edu.mx>
2021-09-08 11:43:34 -0500

    Tortillas

---
+++ tortillas.js
@@ -0,0 +1,15 @@
+var tortilleria = "cerrado";
+var tortilleria2 = "abierto"; 
+
+console.log("Paso 1.- Salir a la entrada de mi casa");
+console.log("Paso 2.- Dirigirme a la tortilleria mas cercana");
+if(tortilleria == "abierto"){
+    console.log("Paso 3.- Llegar a la tortilleria");
+}else{
+    console.log("Paso 3.- Buscar otra toritilleria");
+}
+if(tortilleria2 == "abierto"){
+    console.log("paso 3.- Llegar a la tortilleria");
+}
+console.log("Paso 4.- Comprar las tortillas");
+console.log("Paso 5.- Caminar de regreso a casa");
