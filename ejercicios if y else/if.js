f7182e37a8c114ebb19ba94a00a17a9154f8cf78
BrianOMora <brian_1321124075@uptecamac.edu.mx>
2021-09-16 14:07:13 -0500

    Ejercicios 1

---
+++ ejercicios if y else/if.js
@@ -0,0 +1,48 @@
+// Ejercicio 1
+var mascota = "Jupiter";
+
+if (mascota == "Jupiter"){
+    console.log("Ese es el nombre de mi gato!!!");
+    } else{
+        console.log("Ese no es el nombre de mi gato");
+}
+
+// Ejercicio 2
+var monto = 2560;
+
+if (monto != 3000){
+    console.log("Falta Dinero");
+    } else{
+        console.log("El dinero esta completo");
+}
+
+// Ejercicio 3
+
+var persona1 = "Luis";
+var persona2 = "Julian";
+
+if (persona1 == persona2){
+    console.log("Ese es mi hermano");
+    } else{
+        console.log("Ese es mi primo");
+}
+
+// Ejercicio 4
+
+var dinero = 15;
+
+if (dinero >= 12){
+    console.log("Comprar los chicles");
+    } else{
+        console.log("No me alcanza para los chicles");
+}
+
+// Ejercicio 5
+
+var conejo = 13;
+
+if (conejo <= 20){
+    console.log("No tenemos mas de 20 conejos");
+    } else{
+        console.log("Tenemos menos de 20 conejos");
+}
---
+++ ejercicios if y else/switch.js
@@ -0,0 +1,109 @@
+// Ejercicio 1
+
+var fruta = "roca";
+
+switch (fruta){
+
+    case "pera":
+           console.log("Comer la pera"); 
+    break;
+
+    case "platano":
+           console.log("Comer el platano");
+    break;
+
+    case "manzana":
+          console.log("Comer la manzana");
+    break;
+
+    default:
+          console.log("No hay esa fruta");
+}
+
+// Ejercicio 2
+
+var mascota = "perro";
+
+switch (mascota){
+
+    case "perro":
+           console.log("Comprar el perro"); 
+    break;
+
+    case "loro":
+           console.log("Comprar el loro");
+    break;
+
+    case "conejo":
+          console.log("Comprar el conejo");
+    break;
+
+    default:
+          console.log("No hay una mascota que quiera");
+}
+
+// Ejercicio 3
+
+var dinero = 350;
+
+switch (dinero){
+
+    case 1300:
+           console.log("Comprar un mando de consola"); 
+    break;
+
+    case 1000:
+           console.log("Comprar un juego");
+    break;
+
+    case 350:
+          console.log("Comprar una mochila");
+    break;
+
+    default:
+          console.log("No tenemos dinero para nada");
+}
+
+// Ejercicio 4
+
+var limpiar = 2;
+
+switch (limpiar){
+
+    case 1:
+           console.log("Limpiar el baño"); 
+    break;
+
+    case 2:
+           console.log("Lavar la ropa");
+    break;
+
+    case 3:
+          console.log("Limpiar el coche");
+    break;
+
+    default:
+          console.log("Hemos limpiado todo");
+}
+
+// Ejercicio 5
+
+var amigos = "Gabriel";
+
+switch (amigos){
+
+    case "Gabriel":
+           console.log("Si, el es mi amigo Gabriel"); 
+    break;
+
+    case "Samael":
+           console.log("Si, el es mi amigo Samael");
+    break;
+
+    case "Dejanny":
+          console.log("Si, ella es mi amiga Dejanny");
+    break;
+
+    default:
+          console.log("No, no conozco a alguien con ese nombre");
+}
