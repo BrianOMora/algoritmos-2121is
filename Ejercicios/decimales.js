2caf484ba29ac6dce7d0e17ccc8108224feabc93
BrianOMora <brian_1321124075@uptecamac.edu.mx>
2021-09-22 18:23:58 -0500

    Ejercicios 22/09/2021

---
+++ Ejercicios/decimales.js
@@ -0,0 +1,12 @@
+var decimales = 0;
+var resultado;
+
+for(var i = 0; i <= 9; i++){
+    
+    decimales = decimales + 0.1;
+    decimales = decimales - 1;
+    decimales++;
+
+    console.log(decimales);
+}
+
