c1f4f4b457c193fe6acf52e4d203ac7f841d7ffe
BrianOMora <brian_1321124075@uptecamac.edu.mx>
2021-09-22 19:08:54 -0500

    Ejercicios 22/09/2021

---
+++ Ejercicios/correlativos.js
@@ -0,0 +1,9 @@
+numero=parseInt(prompt("Ingresa el numero que desa saber sus correlativos")); 
+correlativo = numero;
+
+for(var i = 0; i <= correlativo; i++){
+    
+    console.log(numero);
+    numero = numero * 2; 
+    numero--;
+ }
---
+++ Ejercicios/sucesion.js
@@ -0,0 +1,8 @@
+var numeros = 5;
+
+for(var i = 0; i <= 20; i++){
+    console.log(numeros);
+    numeros = numeros + 2; 
+    numeros++;
+    i = numeros; 
+}
